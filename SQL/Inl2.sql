4fcb25b0629d2c8691c9b897cd8058c2e771c2e4
glindouf <gustavlindouf@gmail.com>
2016-03-03 09:48:08 +0100

    Svar i bokstavsordning.
    Svaren skrivs i bokstavsordning på fråga 7, 12 och 16.

--- SQL/Inl2.sql
+++ SQL/Inl2.sql
@@ -10,7 +10,7 @@ CREATE VIEW v5 as SELECT uv.namn FROM uv, k, sp WHERE uv.kkod = (SELECT k.kkod W
 
 CREATE VIEW v6 as SELECT sport FROM sp group by sport having count()=1;
 
-CREATE VIEW v7 as SELECT distinct k.knamn FROM (((((k join uv on k.kkod=uv.kkod) join pb on pb.namn = uv.namn) join b on b.sysnr=pb.sysnr) join sp on sp.namn=pb.namn) join f on f.namn=sp.namn) WHERE b.bryggeri='Epic' and sp.sport='Pole dance' and f.ford='GL';
+CREATE VIEW v7 as select distinct k.knamn from (((((k join uv on k.kkod=uv.kkod) join pb on pb.namn = uv.namn) join b on b.sysnr=pb.sysnr) join sp on sp.namn=pb.namn) join f on f.namn=sp.namn) where b.bryggeri='Epic' and sp.sport='Pole dance' and f.ford='GL' order by k.knamn;
 
 CREATE VIEW v8 as SELECT namn FROM uv group by namn having count(namn AND kkod)=1;
 
@@ -20,15 +20,14 @@ CREATE VIEW v10 as SELECT p.namn, px.namn, p.rum, p.tel, px.tel FROM p, p px WHE
 
 CREATE VIEW v11 as SELECT p.tel FROM ((p join pb on p.namn = pb.namn) join b on b.sysnr = pb.sysnr) group by tel having count(land) =1;  	-->		//	EJ FÄRDIG - SKRIVER UT FYRA/FEM
 
-
-CREATE VIEW v12 as SELECT distinct knamn FROM (k join uv on k.kkod = uv.kkod) WHERE uv.namn = ’Lennart’ and uv.kkod not in (SELECT kkod FROM uv WHERE namn = ‘Kjell’ or namn = ‘Marie’);
+CREATE VIEW v12 as SELECT distinct knamn from (k join uv on k.kkod = uv.kkod) where uv.namn = ’Lennart’ and uv.kkod not in (select kkod from uv where namn = ‘Kjell’ or namn = ‘Marie’) order by knamn;
 
 CREATE VIEW v13 as SELECT namn FROM sp WHERE sport = ‘Jogging’ and namn not in (SELECT namn FROM f WHERE ford = ‘Bil’) and namn in (SELECT namn FROM pb WHERE sysnr in (SELECT sysnr FROM b WHERE land=’Danmark’));
 
 
 CREATE VIEW v15 as SELECT distinct uv.namn, k.knamn FROM ((uv join tf on uv.kkod = tf.kkod AND uv.termin = tf.termin) join k on k.kkod = tf.kkod) WHERE antal >= (SELECT max(antal) FROM tf);
 
-CREATE VIEW v16 as SELECT beer FROM b natural join pb natural join uv natural join (SELECT * FROM tf WHERE antal = (SELECT min (antal) ant FROM tf WHERE antal > (SELECT min (antal) FROM tf)));
+CREATE VIEW v16 as SELECT distinct beer FROM b natural join pb natural join uv natural join (SELECT * FROM tf WHERE antal = (SELECT min (antal) ant FROM tf WHERE antal > (SELECT min (antal) FROM tf))) order by beer;
 
 CREATE VIEW V17 as SELECT distinct rum FROM (p join uv on p.namn = uv.namn) WHERE p.namn not in(SELECT kansv FROM tf); 
 
